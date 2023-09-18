ace68e011df762df38ddd5b6a4211ee2e9fa312b
deanyang122 <97731209+shengshengyang@users.noreply.github.com>
2023-09-18 15:45:20 +0800

    hotfix

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 # start Docker daemon
 sudo systemctl start docker
+
+# remove unused Docker objects
+docker system prune -a -f
 
 # build the Docker image
 docker build -t openaichat .
