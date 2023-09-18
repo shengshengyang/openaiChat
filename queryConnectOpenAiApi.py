c1dd5ac81fb39364f2e3ac4701a3af6ab87e3a4e
deanyang122 <i64991189@gs.ncku.edu.tw>
2023-09-18 22:10:14 +0800

    hotfix

--- queryConnectOpenAiApi.py
+++ queryConnectOpenAiApi.py
@@ -49,7 +49,7 @@ def handle_query():
     def replace_none_with_na(value):
         return '無' if pd.isnull(value) else value
 
-    matched_data = matched_data.map(replace_none_with_na)
+    matched_data = matched_data.applymap(replace_none_with_na)
     top_results_str = json.dumps(json.loads(matched_data.to_json(orient='records')), ensure_ascii=False)
 
     api_endpoint = "https://api.openai.com/v1/chat/completions"
