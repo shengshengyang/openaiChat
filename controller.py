e7bc8264c420d1f8cb8535689fa231b24ba1115e
deanyang122 <111415664+deanyang122@users.noreply.github.com>
2023-11-20 15:14:02 +0800

    add query api doc

--- controller.py
+++ controller.py
@@ -32,6 +32,24 @@ def login():
 
 @app.route('/login', methods=['POST'])
 def show_login():
+    """
+     Show login
+     ---
+     consumes:
+        - application/json
+     parameters:
+       - name: body
+         in: body
+         type: string
+         required: true
+         description: Account
+         example: '{"account": "admin", "password": "2759"}'
+     responses:
+       200:
+         description: Login successful
+       401:
+         description: Unauthorized
+"""
     expected_account = request.json.get('account', None)
     expected_password = request.json.get('password', None)
 
@@ -49,6 +67,50 @@ def show_login():
 @app.route('/query', methods=['POST'])
 @jwt_required()
 def handle_query():
+    """
+Handle user queries
+---
+tags:
+  - Query
+parameters:
+  - name: Authorization
+    in: header
+    type: string
+    required: true
+    description: Bearer token for authentication
+  - name: query
+    in: body
+    required: true
+    schema:
+      type: object
+      properties:
+        query:
+          type: string
+          description: The user's query
+responses:
+  200:
+    description: Successful response
+    schema:
+      type: object
+      properties:
+        generated_response:
+          type: string
+          description: The generated response
+        matched_data:
+          type: array
+          items:
+            type: object
+            properties:
+              answer:
+                type: string
+                description: The answer to the question
+              question:
+                type: string
+                description: The question asked
+  401:
+    description: Unauthorized
+"""
+
     query = request.json['query']
     result = process_query(query)
 
@@ -64,41 +126,12 @@ class IndexResource(Resource):
           200:
             description: Index page
         """
-        # Your index resource logic here
         return "Index page"
-
-
-class LoginResource(Resource):
-    def post(self):
-        """
-        Show login
-        ---
-        parameters:
-          - name: account
-            in: query
-            type: string
-            required: true
-            description: Account
-          - name: password
-            in: query
-            type: string
-            required: true
-            description: Password
-        responses:
-          200:
-            description: Login successful
-          401:
-            description: Unauthorized
-        """
-        # Your login resource logic here
-        return "Login successful"
-
 
 # Define other API resources in a similar manner
 
 
 api.add_resource(IndexResource, '/index')
-api.add_resource(LoginResource, '/login')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
